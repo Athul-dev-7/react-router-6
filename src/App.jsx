6529d59b643352c8a3bbd75c5cc6125d4a735dce
Das <athuldas@hamon.in>
2023-06-05 12:11:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/About";
+import About from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
